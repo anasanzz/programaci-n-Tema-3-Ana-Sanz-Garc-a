88b45e62f1e6d4fa7cd4d61dc473167e6537a970
Ana Sanz García <asangar513e@g.educaand.es>
2025-01-31 11:16:24 +0100

    Add files via upload

---
+++ programación/actividad 3/js/script.js
@@ -0,0 +1,7 @@
+function pedirDatos() {
+    let nombre;
+    let edad;
+    nombre= prompt("¿Cómo te llamas?");
+    edad= prompt("¿Qué edad tienes?");
+    alert("Hola "+nombre+", veo que tienes "+edad+ " años");
+}
