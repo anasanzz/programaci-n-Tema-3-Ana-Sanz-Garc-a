a5d692898645f1e793f4e54507d8a1f703c7b5ed
Ana Sanz García <asangar513e@g.educaand.es>
2025-02-26 21:01:14 +0100

    Delete programación/actividad 9 directory

--- programación/actividad 9/js/script.js
+++
@@ -1,8 +0,0 @@
-function Factorial() {
-    let resultado = 1;
-    let numero = parseInt(document.getElementById("caja").value);
-    for (let interacion = 1; interacion<= numero; interacion++){
-      resultado = resultado * interacion
-    }
-    document.getElementById("resultado").innerHTML = "El factorial de " + numero + "es " + resultado
-    }
