f0b02db78c64fcb3723f3da5b2f6453e413ead2d
Ana Sanz García <asangar513e@g.educaand.es>
2025-02-26 23:39:18 +0100

    Update script.js

--- programación/actividad 9/js/script.js
+++ programación/actividad 9/js/script.js
@@ -1,7 +1,7 @@
 function Factorial() {
     let resultado = 1;
     let numero = parseInt(document.getElementById("caja").value);
-    for (let interacion = 1; interacion<= numero; interacion = interacion + 1);{
+    for (let interacion = 1; interacion<= numero; interacion = interacion + 1){
       resultado = resultado * interacion;
     }
     document.getElementById("resultado").innerHTML = "El factorial de " + numero + "es " + resultado;
